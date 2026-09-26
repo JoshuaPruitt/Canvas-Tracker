e41c6d7f07895f0da7276304adc680ad20f7b1da
Joshua Pruitt <joshuapruitt6484@gmail.com>
2026-09-25 20:14:14 -0700

    Setting up apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
+import { useQuery } from '@apollo/client/react';
 import './App.css'
+import GET_LOCATIONS from './components/test.tsx';
+
+const DisplayTest: any = () => {
+  const { loading, error, data}: any = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>...loading</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return data.locations.map(({id, name, description}: any) => (
+    <div>
+      <h1>{id}</h1>
+      <h2>{name}</h2>
+      <p>{description}</p>
+    </div>
+  ))
+}
 
 function App() {
 
   return (
     <>
-      <h1>hi girl</h1>
+      <DisplayTest/>
     </>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+import { ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+  link: new HttpLink({uri: 'http://localhost:5173/apollo'}),
+  cache: new InMemoryCache()
+});
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+);
+
+export default client;
